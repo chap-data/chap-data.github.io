a342b19b0fed27c545835519dfba18c136483111
rickeryan <bayberryan@hotmail.com>
2025-05-03 19:16:01 +0800

    初版页面包括类型和日期选择

---
+++ script.js
@@ -0,0 +1,157 @@
+
+var isImagery = true; // 当前是否为影像图
+var imageryLayer = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}')
+var streetLayer = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}')
+// 初始化地图
+var map = L.map('map', {
+    // crs: L.CRS.EPSG4326, 
+    zoomControl: false,
+    minZoom: 4,
+}).setView([34, 105], 4); // 设置初始中心点和缩放级别
+streetLayer.addTo(map); // 添加影像图层
+var indexData=null; // 用于存储索引数据
+var selectDay=""; // 用于存储选择的日期
+var selectType="O3"; // 用于存储选择的类型
+const imageBounds = [[54.0, 72.0], [11.5, 135.5]]; // 图片覆盖范围
+var currentOverlay; // 用于存储当前的图片覆盖层
+
+/***********************************事件定义********************************************/
+// 全图范围事件
+document.getElementById('reset-view').addEventListener('click', function () {
+    map.setView([34, 105], 4)
+});
+// 底图切换事件
+document.getElementById('toggle-basemap').addEventListener('click', function () {
+    if (isImagery) {
+        map.removeLayer(imageryLayer);
+        streetLayer.addTo(map);
+    } else {
+        map.removeLayer(streetLayer);
+        imageryLayer.addTo(map);
+    }
+    isImagery = !isImagery; // 切换状态
+});
+
+// 监听日期选择框的变化
+document.getElementById('date-picker').addEventListener('change', function (event) {
+    const selectedDate = event.target.value; // 获取用户选择的日期
+
+    // 根据选择的日期执行相应逻辑
+    if (selectedDate ) {
+        selectDay=selectedDate.replace(/-/g, '');; // 更新选择的日期
+        if (selectDay in indexData) {
+            loadData(selectDay); // 重新加载数据
+        }else{
+            alert('数据不存在，请选择其他日期！'); 
+        }
+    }
+});
+// 滑动条切换事件
+document.getElementById('timeline-slider').addEventListener('input', function () {
+    const slider = document.getElementById('timeline-slider');
+    const label = document.getElementById('timestamp');
+    const index = parseInt(slider.value, 10);
+    const dayhour = selectDay+indexData[selectDay][index];
+    label.innerHTML = dayhour;
+    updateImageOverlay(dayhour); // 更新地图图片
+});
+
+/*
+ * 加载json数据
+ */
+function loadData(dayStr) {
+    // 根据气体类型确定json路径
+    let index_url=""
+    if(selectType=="NO2"){
+        index_url="./NO2/data/sta/index.json"
+    }else{
+        index_url="./O3/data/sta/index.json"
+    }
+    // 加载渲染json
+    fetch(index_url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(index => {
+            indexData = index; // 存储索引数据
+            // 获取滑动条和标签元素
+            const slider = document.getElementById('timeline-slider');
+            const label = document.getElementById('timestamp');
+            slider.value = 0; // 设置初始值为0
+            lastDayStr=Object.keys(indexData).pop();
+            if (dayStr in indexData) {
+                selectDay=dayStr 
+            }else{
+                selectDay=lastDayStr // 如果没有传入日期，则使用最后一天的日期
+            }
+            hours=indexData[selectDay]
+            slider.max = hours.length - 1; // 设置滑动条的最大值
+            dayhour=selectDay+hours[0]; // 获取第一个时间戳
+            label.innerHTML = dayhour;
+            updateImageOverlay(dayhour); // 初始化地图图片
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+        });
+}
+
+
+/*
+ * 更新渲染图
+ */
+function updateImageOverlay(dayhour) {
+    let imgUrl=""
+    if(selectType=="NO2"){
+        imgUrl=`./NO2/data/png/Y${dayhour.slice(0,4)}-M${dayhour.slice(4,6)}-D${dayhour.slice(6,8)}/CHAP_NRT_NO2_${dayhour}00.png`;
+    }else{
+        imgUrl=`./O3/data/png/Y${dayhour.slice(0,4)}-M${dayhour.slice(4,6)}-D${dayhour.slice(6,8)}/CHAP_NRT_O3_${dayhour}00.png`;
+    }
+    // 如果已有覆盖层，先移除
+    if (currentOverlay) {
+        map.removeLayer(currentOverlay);
+    }
+    // 添加新的图片覆盖层
+    currentOverlay = L.imageOverlay(imgUrl, imageBounds).addTo(map);
+}
+
+
+/*
+ * 加载区划geojson
+ */
+function loadGeoJSON() {
+    fetch('./prov.json') // 替换为你的 GeoJSON 文件路径
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Failed to load GeoJSON');
+        })
+        .then(geojsonData => {
+            // 使用 L.geoJSON 将 GeoJSON 数据添加到地图
+            L.geoJSON(geojsonData, {
+                style: function (feature) {
+                    return {
+                        color: '#1e78dc', // 边界颜色
+                        weight: 1, // 边界宽度
+                        fillOpacity: 0.1, // 填充透明度
+                    };
+                },
+                onEachFeature: function (feature, layer) {
+                    // 为每个 GeoJSON 特征添加弹窗
+                    if (feature.properties && feature.properties.name) {
+                        layer.bindPopup(`${feature.properties.name}`);
+                    }
+                }
+            }).addTo(map);
+        })
+        .catch(error => {
+            console.error('Error loading GeoJSON:', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadData();
+    loadGeoJSON();
+});
