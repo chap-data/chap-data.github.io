c204b13fca1edcc504033c35767498ff08ff0bd2
rickeryan <bayberryan@hotmail.com>
2025-05-04 22:44:20 +0800

    新增类型切换对应的数据和界面变更；

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var map = L.map('map', {
 streetLayer.addTo(map); // 添加影像图层
 var indexData=null; // 用于存储索引数据
 var selectDay=""; // 用于存储选择的日期
-var selectType="O3"; // 用于存储选择的类型
+var selectType="type-o3"; // 用于存储选择的类型
 const imageBounds = [[54.0, 72.0], [11.5, 135.5]]; // 图片覆盖范围
 var currentOverlay; // 用于存储当前的图片覆盖层
 
@@ -31,6 +31,18 @@ document.getElementById('toggle-basemap').addEventListener('click', function () 
     }
     isImagery = !isImagery; // 切换状态
 });
+// 气体类型切换事件
+function changeGasType(event) {
+    document.getElementById('type-o3').className='type-button'
+    document.getElementById('type-pm2.5').className='type-button'
+    document.getElementById('type-no2').className='type-button'
+    event.target.classList.add('selected')
+    selectType=event.target.id; // 获取用户选择的气体类型
+    loadData()
+}
+document.getElementById('type-o3').addEventListener('click', changeGasType);
+document.getElementById('type-pm2.5').addEventListener('click', changeGasType);
+document.getElementById('type-no2').addEventListener('click', changeGasType);
 
 // 监听日期选择框的变化
 document.getElementById('date-picker').addEventListener('change', function (event) {
@@ -52,7 +64,7 @@ document.getElementById('timeline-slider').addEventListener('input', function ()
     const label = document.getElementById('timestamp');
     const index = parseInt(slider.value, 10);
     const dayhour = selectDay+indexData[selectDay][index];
-    label.innerHTML = dayhour;
+    label.innerHTML = `${dayhour.slice(0,4)}-${dayhour.slice(4,6)}-${dayhour.slice(6,8)} ${dayhour.slice(-2)}:00`;
     updateImageOverlay(dayhour); // 更新地图图片
 });
 
@@ -62,7 +74,9 @@ document.getElementById('timeline-slider').addEventListener('input', function ()
 function loadData(dayStr) {
     // 根据气体类型确定json路径
     let index_url=""
-    if(selectType=="NO2"){
+    if(selectType=="type-pm2.5"){
+        index_url="./PM25/data/sta/index.json"
+    }else if(selectType=="type-no2"){
         index_url="./NO2/data/sta/index.json"
     }else{
         index_url="./O3/data/sta/index.json"
@@ -79,7 +93,6 @@ function loadData(dayStr) {
             // 获取滑动条和标签元素
             const slider = document.getElementById('timeline-slider');
             const label = document.getElementById('timestamp');
-            slider.value = 0; // 设置初始值为0
             lastDayStr=Object.keys(indexData).pop();
             if (dayStr in indexData) {
                 selectDay=dayStr 
@@ -88,9 +101,11 @@ function loadData(dayStr) {
             }
             hours=indexData[selectDay]
             slider.max = hours.length - 1; // 设置滑动条的最大值
-            dayhour=selectDay+hours[0]; // 获取第一个时间戳
-            label.innerHTML = dayhour;
+            slider.value = slider.max; // 设置初始值为0
+            dayhour=selectDay+hours[slider.max]; // 获取第一个时间戳
+            label.innerHTML = `${dayhour.slice(0,4)}-${dayhour.slice(4,6)}-${dayhour.slice(6,8)} ${dayhour.slice(-2)}:00`;
             updateImageOverlay(dayhour); // 初始化地图图片
+            updateLegendLabel(); // 更新图例标签
         })
         .catch(error => {
             console.error('Error fetching JSON:', error);
@@ -103,7 +118,9 @@ function loadData(dayStr) {
  */
 function updateImageOverlay(dayhour) {
     let imgUrl=""
-    if(selectType=="NO2"){
+    if(selectType=="type-pm2.5"){
+        imgUrl=`./PM25/data/png/Y${dayhour.slice(0,4)}-M${dayhour.slice(4,6)}-D${dayhour.slice(6,8)}/CHAP_NRT_NO2_${dayhour}00.png`;
+    }else if(selectType=="type-no2"){
         imgUrl=`./NO2/data/png/Y${dayhour.slice(0,4)}-M${dayhour.slice(4,6)}-D${dayhour.slice(6,8)}/CHAP_NRT_NO2_${dayhour}00.png`;
     }else{
         imgUrl=`./O3/data/png/Y${dayhour.slice(0,4)}-M${dayhour.slice(4,6)}-D${dayhour.slice(6,8)}/CHAP_NRT_O3_${dayhour}00.png`;
@@ -116,6 +133,39 @@ function updateImageOverlay(dayhour) {
     currentOverlay = L.imageOverlay(imgUrl, imageBounds).addTo(map);
 }
 
+/*
+ *根据气体类型更新图例   
+*/
+function updateLegendLabel() {
+    if (selectType == "type-pm2.5") {
+        document.getElementById('gas-label').innerHTML = 'PM2.5(ug/m3)';
+        document.getElementById('tick0').innerHTML = '25';
+        document.getElementById('tick1').innerHTML = '50';
+        document.getElementById('tick2').innerHTML = '75';
+        document.getElementById('tick3').innerHTML = '100';
+        document.getElementById('tick4').innerHTML = '125';
+        document.getElementById('tick5').innerHTML = '150';
+        document.getElementById('tick6').innerHTML = '175';
+    }else if (selectType == "type-no2") {
+        document.getElementById('gas-label').innerHTML = 'NO2(ug/m3)';
+        document.getElementById('tick0').innerHTML = '25';
+        document.getElementById('tick1').innerHTML = '50';
+        document.getElementById('tick2').innerHTML = '75';
+        document.getElementById('tick3').innerHTML = '100';
+        document.getElementById('tick4').innerHTML = '125';
+        document.getElementById('tick5').innerHTML = '150';
+        document.getElementById('tick6').innerHTML = '175';
+    }else{
+        document.getElementById('gas-label').innerHTML = 'O3(ug/m3)';
+        document.getElementById('tick0').innerHTML = '25';
+        document.getElementById('tick1').innerHTML = '50';
+        document.getElementById('tick2').innerHTML = '75';
+        document.getElementById('tick3').innerHTML = '100';
+        document.getElementById('tick4').innerHTML = '125';
+        document.getElementById('tick5').innerHTML = '150';
+        document.getElementById('tick6').innerHTML = '175';
+    }
+}
 
 /*
  * 加载区划geojson
